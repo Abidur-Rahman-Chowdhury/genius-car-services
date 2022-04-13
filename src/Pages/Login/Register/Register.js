50056b6ca3c67bbb137344fa16b2e0bcefb7da96
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-13 13:52:44 +0600

    done login and register component

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
-    return (
-        <div>
-            
+
+    const handelRegister = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        
+
+   }
+    return ( 
+        <div className='register-form'>
+            <h2 className='text-center text-primary mt-5'>Please Register</h2>
+            <form onSubmit={handelRegister}>
+                <input type="text" name="name" placeholder='Your Name' required />
+                
+                <input type="email" placeholder='Your Email' required name="email" id="" />
+                
+                <input type="password" placeholder='Your Password' required name="password" id="" />
+                
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger text-decoration-none ' style={{ cursor: "pointer" }} >Login</Link></p>
         </div>
     );
 };
