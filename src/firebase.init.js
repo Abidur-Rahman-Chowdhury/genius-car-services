a756ea433b8a0b8885b76c7df609368b52dfbb1c
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-13 14:07:22 +0600

    set environmet variable

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAVvAbYNtQquSirmX730qk5KwhHS5U8QG0",
-  authDomain: "genius-car-services-5b346.firebaseapp.com",
-  projectId: "genius-car-services-5b346",
-  storageBucket: "genius-car-services-5b346.appspot.com",
-  messagingSenderId: "1219455500",
-  appId: "1:1219455500:web:1d87fece181bf9de215c29"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
