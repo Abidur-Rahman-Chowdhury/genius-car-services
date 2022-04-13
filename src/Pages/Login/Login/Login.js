8dda0d0aee44ff7e5f87a12e57c7ed5aeebd79d9
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-13 14:50:57 +0600

    done login and register functionality using react-firebase-hooks

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,18 +1,30 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
 
     const handelSubmit = (e) => {
         e.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
+        signInWithEmailAndPassword(email, password);
         console.log(email,password);
         
+    }
+    if (user) {
+        navigate('/home')
     }
   return (
       <div className='container w-50 mx-auto'>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] =useCreateUserWithEmailAndPassword(auth);
 
     const handelRegister = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        
+        createUserWithEmailAndPassword(email, password);
 
-   }
+
+    }
+    if (user) {
+        navigate('/home');
+    }
     return ( 
         <div className='register-form'>
             <h2 className='text-center text-primary mt-5'>Please Register</h2>
