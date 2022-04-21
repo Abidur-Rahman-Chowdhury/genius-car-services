fec42a55b11c68dbb1edfea02a7e06942312ea01
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-22 03:01:10 +0600

    last update

--- src/Pages/Login/RequiredAuth/RequiredAuth.js
+++ src/Pages/Login/RequiredAuth/RequiredAuth.js
@@ -1,10 +1,20 @@
+import { Toast } from 'bootstrap';
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequiredAuth = ({ children }) => {
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    const handelSendEmail = async () => {
+        await sendEmailVerification();
+          toast('Sent email');
+    }
     const [user,loading] = useAuthState(auth);
     let location = useLocation();
     if (loading) {
@@ -13,7 +23,15 @@ const RequiredAuth = ({ children }) => {
     if (!user) {
         return <Navigate to='/login' state ={{from:location}} replace></Navigate>
     }
+    if (!user.emailVerified) {
+        return <div>
+            <h3 className='text-danger'>Your Email is not Verified!!</h3>
+            <h5 className='text-success'>Please Verify Email</h5>
 
+            <button className='btn btn-primary' onClick={handelSendEmail} >Send Verify Email </button>
+            <ToastContainer></ToastContainer>
+        </div>
+    }
     return children;
 };
 
